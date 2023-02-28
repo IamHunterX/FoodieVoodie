404e309232937485b1a744e09798a4354ca64cf0
IamHunterX <aslamhassam1122@hotmail.com>
2023-02-28 12:07:53 +0500

    Foodie Voodie First commit

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image,StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1,flexDirection:'column'}}>
+      <View style={{backgroundColor:'grey',flex:0.20,justifyContent:'center',alignItems:'center'}}>
+        <Image 
+        style={{width:50,height:50}}
+        source ={require("./assets/Logo.png")}
+        />
+      </View>
+      {/* <Text></Text> */}
+      {/* <View style={{backgroundColor:'blue',flex:0.20}}></View> */}
+      <View style={{backgroundColor:'black',flex:0.60,justifyContent:'center',alignItems:'center'}}>
+        <Text
+        style={{backgroundColor:'#blue',marginBottom:10,fontSize:16}}>Login</Text>
+        <TextInput
+        style={{width:150,height:35,backgroundColor:'white',marginBottom:15}}>
+        </TextInput>
+        <TextInput
+        style={{width:150,height:35,backgroundColor:'white',marginTop:5.0,}}>
+        </TextInput>
+      </View>
+      <View style={{backgroundColor:'green',flex:0.20,justifyContent:'center',alignItems:'center'}}>
+        {/* <Button>"Click"</Button> */}
+        {/* <Button>Primary</Button>   */}
+        <TouchableOpacity style={{width:200,height:35,justifyContent:'center',alignItems:'center'}}>
+          <Text style={{backgroundColor:'orange',fontSize:16,fontStyle:'italic',}}>login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{width:200,height:35,justifyContent:'center',alignItems:'center'}}>
+          <Text style={{backgroundColor:'orange',fontSize:16,fontStyle:'italic'}}>Forget Password?</Text>
+        </TouchableOpacity>
+      </View>
+      <StatusBar style="auto"/>
     </View>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ assets/MyCSS.jsx
@@ -0,0 +1,18 @@
+const imageStyle = {
+    maxWidth: '100%',
+    maxHeight: '100%',
+  };
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      // alignItems: 'center',
+      // justifyContent: 'center',
+    },
+    topView:{backgroundColor:'grey',
+    flex:0.20,justifyContent:'center',
+    alignItems:'center'}
+  });
+
+  export {styles}
+  
